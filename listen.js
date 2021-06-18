042ec6f3df66aedff34456e271b993d5dde0d2fd
PaulOR26 <pj_orourke@outlook.com>
2021-06-18 14:33:50 +0100

    setup heroku

--- listen.js
+++ listen.js
@@ -1,3 +1,7 @@
 const app = require('./app');
+const { PORT = 9090 } = process.env;
 
-app.listen(3000, () => console.log('listening!'));
+app.listen(PORT, (err) => {
+  if (err) throw err;
+  console.log(`listening on ${PORT}!`);
+});
